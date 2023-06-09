9926de0ae06d2425dd5e31599d89db8812a24134
Daxitram <37157568+Daxitram@users.noreply.github.com>
2023-06-09 15:19:33 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,133 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+    <style type="text/css">
+      .pp{
+        width: 200px;
+        height: 200px;
+        border-radius: 50%;
+      }
+      .istos{
+        width: 200px;
+        border-radius: 50%;
+      }
+
+      .box{
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            flex-direction: row;
+
+          }
+      h1{
+        text-shadow: 3px 3px 0 red;
+        color: #FF8A65;
+      }
+      h2{
+        text-shadow: 2px 2px 0 red;
+        color: #FF8A65;
+      }
+      body{
+        background-color: #333 ;
+      }
+      p{
+        margin-left: auto;
+        margin-right: auto;
+        width: 50em;
+        color: #FF8A65;
+      }
+      ul{margin-left: auto;
+        margin-right: auto;
+        width: 50em
+        
+      }
+      li{
+        color: #FF8A65;
+      }
+      *, ::before, ::after{
+        box-sizing: border-box;
+        margin: 0;
+        padding: 0;
+      }
+      h3{
+        text-align: center;
+        color: #f1f1f1;
+      }
+      .galerie{
+        display: flex;
+        flex-wrap: wrap;
+        max-width: 1300px;
+        width: 80%;
+        margin: 70px auto 0;
+      }
+    </style>
+  </head>
+  <body>
+    <div class="box">
+    <img src="istos.png" class="istos">
+    <h1>ISTOS GAMING</h1>
+    </div>
+
+    <div >
+      <br>
+    <h2>Membres:</h2>
+<div class="container">
+    <div class="galerie">
+
+    <div><h3>Daxitram</h3><img src="daxi.jpg" class="pp"></div>
+
+
+   </div>
+      
+ </div>     
+      
+      
+    </div>
+
+<div>
+  <br>
+<h2>C'est quoi Istos Gaming ?</h2>
+  <p>Istos Gaming est une structure E-Sport qui a pour but d'accueillir des joueurs en situation de handicap mais pas seulement. L'un des objectifs est aussi d'apporter une socialisation à des personnes ayant tendance à être isolées.</p>
+  <p>Si le projet voit vraiment le jour, il s'agirait d'une association à but non lucratif. Lees modèle économique serait la vente de T-Shirt et les gains lors des compétitions mais aussi une chaîne youtube et twitch et des sponsors. Pour commencer l'association n'aura pas de siège vraiment précis si ce n'est mon appartement.
+  </p>
+<p>pour lancer le projet j'ai l'équipement nécessaire pour faire une team fortnite, j'ai des solutions pour d'autres jeux mais pour fortnite et autres jeux cross platform c'est concret.</p>
+  <p>
+  Si l'association peut se le permettre il faudra un local.</p><p>
+  Comme c'est une association qui intègrerait des personnes en situation de handicap, il y aurait besoin à terme d'accompagnement si les fonds le permettent notamment: auxiliaire de vie, ergothérapeute, kiné. qui serait salariés. les auxiliaires de vies seraient présente pour les besoins du quotidien. Les ergothérapeutes pour que les installations pour jouer soient le plus adapté et le plus pratique possible. Les kinés pour que les joueurs puissent être massés et étirés pour être plus efficaces, un peu comme les joueurs de foot.
+  l'étape ultime serait d'avoir un ou deux camions pour transporter les joueurs.
+</p>
+</div>
+
+<div>
+  <br>
+<h2>Genèse du projet</h2>
+    <p>la première fois que j'ai eu cette idée je jouais à Fortnite.
+J’ai joué quelques fois avec 3 amis eux aussi handicapé, je leur ai parlé de cette idée mais ils m’ont dit que je rêvais.
+</p>
+<p>En 2022, je me suis mis à World of Warcraft un peu plus sérieusement puis l’idée est revenue, sauf que je n’avais pas d’amis handicapé qui y joue donc c'est compliqué. Ce qui est encore plus problématique est le fait que l’esport sur WOW n’est pas le plus développé,  donc j’ai plus où moins laissé tombé.
+</p>
+<p>
+  Je me suis enfin mis LOL alors que le jeu 14 ans.Je pense que l’esport sur ce jeu est bien démocratisé. 
+  Je ne prétend pas avoir un niveau mais j’ai envie de voir ce projet esport naître.
+</p>
+</div>
+
+<div>
+  <br>
+<h2>Objectifs à réaliser pour vraiment lancer ce projet:</h2>
+<ul>
+    <li>Trouver une base de joueurs pour adhérer au projet</li>
+    <li>Créer une team avec une bonne cohésion</li>
+    <li>Lancer une chaîne twitch pour se faire connaître</li>
+    <li>trouver des sponsors</li>
+    <li>Participer à une compétition IRL et éclater les équipes sans joueurs en situation de handicap</li>
+
+</ul>
+</div>
+
+  </body>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
+</html>
